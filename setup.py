ea880cf25ab250cc71d289fa9e6a787059582443
Sergei Izmailov <sergei.a.izmailov@gmail.com>
2019-11-13 22:39:06 +0300

    remove binfile.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name='amber-runner',
+    maintainer="Sergei Izmailov",
+    maintainer_email="sergei.a.izmailov@gmail.com",
+    description="Batch execution for Amber MD",
+    url="https://github.com/sizmailov/amber-runner",
+    version="0.0.0",
+    long_description=open("README.rst").read(),
+    license="BSD",
+    install_requires=[
+        'remote-runner',
+        'pyxmolpp2',
+        'deprecated',
+        'f90nml'
+    ],
+    tests_require=[
+        'pytest'
+    ],
+    packages=[
+        'amber_runner'
+    ]
+)
