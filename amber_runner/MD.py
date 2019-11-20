6eee5981496b40319384cfdf1aa2f53c58ba69d9
Sergei Izmailov <sergei.a.izmailov@gmail.com>
2019-11-20 20:55:48 +0300

    Introduce superior MdProtocol

--- amber_runner/MD.py
+++ amber_runner/MD.py
@@ -1,13 +1,17 @@
 import os
-import subprocess
 from pathlib import Path
-
+from typing import List, final
 import remote_runner
+from .command import Command
+from .inputs import InputWriter, AmberInput, TleapInput, ParmedInput
+from .executables import PmemdCommand, SanderCommand, ParmedCommand, TleapCommand
+from remote_runner.utility import ChangeDirectory
 
 
 class Step:
     def __init__(self, name):
         self.name = name
+        self.is_complete = False
 
     def run(self, md: 'MD', step_dir: Path):
         raise NotImplementedError()
@@ -19,172 +23,73 @@ class CommandWithInput:
         self.input = inp
 
     def run(self, input_filename: Path, **kwargs):
-        return self.exe.run(inp=self.input, input_filename=input_filename, **kwargs)
+        with input_filename.open("w") as inp:
+            self.input.write(inp)
+        return self.exe.run(**kwargs)
 
 
 class Build(Step):
     def __init__(self, name):
         super().__init__(name)
         self.tleap = CommandWithInput(exe=TleapCommand(), inp=TleapInput())
-        self.parmed = CommandWithInput(exe=ParmedCommand(), inp=ParmedInput())
+        # self.parmed = CommandWithInput(exe=ParmedCommand(), inp=ParmedInput())
 
     def run(self, md: 'MD', step_dir: Path):
         self.tleap.run(step_dir / "tleap.in")
-        self.parmed.run(step_dir / "parmed.in")
-        md.exe.previous_rst = 1
+        # self.parmed.run(step_dir / "parmed.in")
+        md.sander.restrt = step_dir / "frame.prmtop"
 
 
-class Minimize(Step):
+class SingleSanderCall(Step):
     def __init__(self, name):
         super().__init__(name)
-        self.min_1_input = AmberInput()
-        self.min_2_input = AmberInput()
+        self.input = AmberInput()
 
     def run(self, md: 'MD', step_dir: Path):
-        md.exe(output=str(step_dir / "min_1")).run(self.min_1_input, step_dir / "min_1.in")
-        md.exe.run(self.min_2_input, step_dir / "min_2.in")
+        with md.sander.scope_args(output_prefix=str(step_dir / self.name)) as exe:
+            exe.run(self.input, step_dir / f"{self.name}.in")
 
 
-class Equilibrate(Step):
-    pass
+class RepeatedSanderCall(Step):
+    def __init__(self, name, number_of_steps: int):
+        super().__init__(name)
+        self.input = AmberInput()
+        self.current_step = 0
+        self.number_of_steps = number_of_steps
+
+    def run(self, md: 'MD', step_dir: Path):
+        while self.current_step < self.number_of_steps:
+            with md.sander.scope_args(output_prefix=str(step_dir / f"{self.name}{self.current_step:05d}")) as exe:
+                exe.run(self.input, step_dir / f"{self.name}{self.current_step:05d}.in")
+            self.current_step += 1
+            md.checkpoint()
 
 
-class ProductionRun(Step):
-    pass
-
-
-class MD(remote_runner.Task):
+class MdProtocol(remote_runner.Task):
 
     def __init__(self, name: str, wd: Path):
         super().__init__(wd=wd)
         self.name = name
+        self.sander: SanderCommand = PmemdCommand()
 
-        self.exe: SanderCommand = PmemdCommand()
-
-        self.min1 = AmberInput()
-        self.min2 = AmberInput()
-        self.heat = AmberInput()
-        self.equil = AmberInput()
-        self.run = AmberInput()
-
-        self.reference = None
-        self.restart = None
-        self.prmtop = None
-
-    def mkdir_p(self, path):
+    @staticmethod
+    def mkdir_p(path):
         if not os.path.isdir(path):
-            self.log("mkdir %s" % path)
             os.mkdir(path)
 
-    def minimize(self):
-        self.mkdir_p(MD._min_dir)
-
-        self.min1_parameters.write_to(MD._min_dir + "/min_1")
-        self.call_pmemd(MD._min_dir + "/min_1")
-
-        self.min2_parameters.write_to(MD._min_dir + "/min_2")
-        self.call_pmemd(MD._min_dir + "/min_2")
-
-    def heat(self):
-        self.mkdir_p(MD._heat_dir)
-        self.heat_parameters.write_to(MD._heat_dir + "/heat")
-        self.call_pmemd(MD._heat_dir + "/heat")
-
-    def equilibrate(self):
-        self.mkdir_p(MD._equilibration_dir)
-        self.equil_parameters.write_to(MD._equilibration_dir + "/equil")
-        self.call_pmemd(MD._equilibration_dir + "/equil")
-
     @property
-    def required_steps(self):
-        if self.current_step > 0:
-            return [self.current_step - 1]
-        else:
-            return []
-
-    def do_md_step(self):
-        next_step = self.current_step + 1
-        self.mkdir_p(MD._run_dir)
-
-        step_prefix = self._run_dir + "/run" + MD._pattern % next_step
-        self.run_parameters.write_to(step_prefix)
-        self.call_pmemd(step_prefix)
-
-        self.current_step = next_step
-        self.save()
-
-    def run_setup(self):
+    def steps(self) -> List[Step]:
         raise NotImplementedError()
 
-    def run_continue(self):
-        raise NotImplementedError()
-
+    @final
     def run(self):
-        if not self.setup_is_done:
-            self.run_setup()
-        self.run_continue()
+        for i, step in enumerate(self.steps):
+            with ChangeDirectory():
+                wd = f"{1 + i}_{step.name}"
+                self.mkdir_p(wd)
+                step.run(self, Path(wd))
+                self.checkpoint()
 
-    def call_cmd(self, cmd):
-        output = None
-        try:
-            output = subprocess.check_output(cmd)
-        except subprocess.CalledProcessError as e:
-            self.log("Process " + " ".join(cmd) + " returned non zero status " + str(e.returncode) + "."
-                                                                                                     "with message" + e.message,
-                     MD.ERROR)
-            if abort_on_failure:
-                self.log("Output: " + e.output, MD.FATAL)
-            else:
-                return e.returncode, e.output
-        except Exception as e:
-            self.log("Unspecified failure " + e.message + "on command " + " ".join(cmd) + ". Abort.", MD.FATAL)
+    def checkpoint(self):
+        self.save(self.state_filename)
 
-        # 0 -- success code
-        return 0, output
-
-    def call_cmd_pipe(self, cmd, stdin):
-        import subprocess
-        proc = subprocess.Popen(cmd, stdin=subprocess.PIPE)
-        try:
-            o, e = proc.communicate(stdin)
-        except subprocess.CalledProcessError as e:
-            self.log("Process " + " ".join(cmd) + " returned non zero status " + str(e.returncode) + "."
-                                                                                                     "with message" + e.message,
-                     MD.ERROR)
-            self.log("Output: " + e.output, MD.FATAL)
-        except Exception as e:
-            self.log("Unspecified failure " + e.message + "on command " + " ".join(cmd) + ". Abort.", MD.FATAL)
-
-        return 0, o
-
-    def run_pmemd(self):
-        cmd = self.pmemd_executable + [
-            "-O",
-            "-i", prefix + ".in",
-            "-o", prefix + ".out",
-            "-p", self.prmtop,
-            "-c", self.restart,
-            "-ref", self.reference,
-            "-x", prefix + ".nc",
-            "-r", prefix + ".rst",
-            "-inf", prefix + ".mdinfo",
-            "-l", prefix + ".log"
-        ]
-        self.call_cmd(cmd)
-
-    def _on_fatal(self):
-        import sys
-        self.check_wd()
-        self.save_state("fatal_state.pickle")
-        sys.exit(1)
-
-    def set_step_as_restart_file(self, step_number=-1):
-        """
-        :param step_number:
-        :type step_number: int
-        :return:
-        """
-        if step_number < 0:
-            step_number = self.current_step + step_number + 1
-
-        self._restart_filename = MD._run_dir + "/" + "run" + MD._pattern % step_number + ".rst"
