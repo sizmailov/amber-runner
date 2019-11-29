216d34a1df157d16c20c591b2f892e911fb6a92a
Sergei Izmailov <sergei.a.izmailov@gmail.com>
2019-11-29 13:30:44 +0300

    Release 0.0.1
     - change MdProtocol.mkdir

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     maintainer_email="sergei.a.izmailov@gmail.com",
     description="Batch execution for Amber MD",
     url="https://github.com/sizmailov/amber-runner",
-    version="0.0.0",
+    version="0.0.1",
     long_description=open("README.rst").read(),
     license="BSD",
     install_requires=[
